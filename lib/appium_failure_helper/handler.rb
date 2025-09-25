577eea9366c7987d7a939ed853ad716331beaf90
David Nascimento <halison700@gmail.com>
2025-09-24 22:44:03 -0300

    Corrigido geração de relatorio em HTML

--- lib/appium_failure_helper/handler.rb
+++ lib/appium_failure_helper/handler.rb
@@ -17,28 +17,17 @@ module AppiumFailureHelper
       platform = @driver.capabilities['platformName']&.downcase || 'unknown'
 
       failed_info = Analyzer.extract_failure_details(@exception)
-      
-      # ALTERADO: Agora busca em todas as fontes de dados
-      logical_name_key = failed_info[:selector_value].to_s.gsub(/^#/, '')
-      
-      # 1. Busca nos arquivos YAML dinâmicos
-      element_map_yaml = ElementRepository.load_all_from_yaml
-      de_para_yaml_result = Analyzer.find_de_para_match(failed_info, element_map_yaml)
+      unified_element_map = ElementRepository.load_all
+      de_para_result = Analyzer.find_de_para_match(failed_info, unified_element_map)
 
-      # 2. Busca no arquivo Ruby
-      de_para_rb_result = ElementRepository.find_in_ruby_file(logical_name_key)
-
-      # 3. Analisa a tela atual
       page_analyzer = PageAnalyzer.new(page_source, platform)
       all_page_elements = page_analyzer.analyze
       similar_elements = Analyzer.find_similar_elements(failed_info, all_page_elements)
 
-      # Organiza TODOS os resultados para o relatório
       report_data = {
         failed_element: failed_info,
         similar_elements: similar_elements,
-        de_para_yaml_analysis: de_para_yaml_result, # Resultado da análise YAML
-        de_para_rb_analysis: de_para_rb_result,   # Resultado da análise Ruby
+        de_para_analysis: de_para_result,
         all_page_elements: all_page_elements,
         screenshot_base64: @driver.screenshot_as(:base64),
         platform: platform,
@@ -46,7 +35,7 @@ module AppiumFailureHelper
       }
 
       ReportGenerator.new(@output_folder, page_source, report_data).generate_all
-      
+
       Utils.logger.info("Relatórios gerados com sucesso em: #{@output_folder}")
     rescue => e
       Utils.logger.error("Erro ao capturar detalhes da falha: #{e.message}\n#{e.backtrace.join("\n")}")
